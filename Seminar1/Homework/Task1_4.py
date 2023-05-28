cbf8b98fb3342672d5b02ba4ab4930479726911e
aa_georgieva <aa_georgieva@mail.ru>
2023-05-28 22:42:14 +0300

    Task1_4 added

--- Seminar1/Homework/Task1_4.py
+++ Seminar1/Homework/Task1_4.py
@@ -10,7 +10,7 @@ n = int(input())
 m = int(input())
 k = int(input())
 
-if k % n == 0 or k % m == 0 and k < m*n:
+if (k % n == 0 or k % m == 0) and k < m*n:
     print("yes")
 else:
     print("no")
